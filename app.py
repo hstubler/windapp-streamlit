fe60f3107afb206ec50e8c8a9f0c225a46d37f5e
hstubler <86688896+hstubler@users.noreply.github.com>
2023-09-07 17:12:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,55 @@
 import streamlit as st
-import numpy as np
+from influxdb_client import InfluxDBClient
+import warnings
 import pandas as pd
+import arrow
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+org = "a286e8b76dbe1c10"
+bucket = "hstubler"
+# token = 'eN3f7y3tnzWoiJl5sb9-OwhCcGwYluXH37Jm-wYcF_k1cv5QZ7tscq3Gwcw-bb_on-E49pGi69hnxOLq9TCNgA=='
+token = "LQECoOJHlfjBBy51xpOIFy6KSn1R170Nwb1KJw2oHE2-FkbZSAHOOVbQAMDj1K5Up-8ydpnrgw1eE4BsoN_eIQ=="
+url = "http://10.111.0.113:8086"
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+client = InfluxDBClient(url=url, token=token, org=org, timeout=1080_000)
 
-st.line_chart(chart_data)
-
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+time = "2023-06-17T18:30:07.394Z"
 
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+def get_wind_data(time, shift):
+    with open("marks.flux", "r") as file:
+        query = file.read()
+        start = (
+            arrow.get(time).shift(seconds=-10).format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z"
+        )
+        end = (
+            arrow.get(time).shift(seconds=shift).format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z"
+        )
+
+        query = query.replace("{startTime}", start)
+        query = query.replace("{stopTime}", end)
+
+    result = client.query_api().query_data_frame(org=org, query=query)
+    df = result.dropna()
+    df = df.rename(columns={"boat": "mark", "_time": "DATETIME"})[
+        [
+            "DATETIME",
+            "mark",
+            "LATITUDE_MDSS_deg",
+            "LONGITUDE_MDSS_deg",
+            "TWD_MDSS_deg",
+            "TWS_MDSS_km_h_1",
+        ]
+    ]
+    return df
+
+
+df = get_wind_data(time, 50)
+
+# fig = plt.figure(figsize=(10, 4))
+fig = sns.boxplot(
+    data=df, x="TWS_MDSS_km_h_1", y="mark", hue="mark", dodge=False
+).figure
+
+st.pyplot(fig)
